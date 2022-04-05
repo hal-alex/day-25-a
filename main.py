46b7e62201ba28d1cdcc1ffbdf17d222e24082f5
hal-alex <102769562+hal-alex@users.noreply.github.com>
2022-04-05 19:09:12 +0100

    Writing first 2 steps

--- main.py
+++ main.py
@@ -16,8 +16,22 @@ image = "blank_states_img.gif"
 turtle.addshape(image)
 turtle.shape(image)
 
-user_answer = screen.textinput(title="Guess a US state", prompt="Type in below a US state you can think of")
 
+user_answer = screen.textinput(title="Guess a US state", prompt="Type in below a US state you can think of").title()
+
+print(user_answer)
+
+all_of_states = pandas.read_csv("50_states.csv")
+
+list_of_states = all_of_states["state"].to_list()
+
+print(list_of_states)
+
+for state in list_of_states:
+    if user_answer == state:
+        x_coor = all_of_states.query(f"{state}")["x"]
+        print(x_coor)
+        turtle.write(state)
 
 
 
